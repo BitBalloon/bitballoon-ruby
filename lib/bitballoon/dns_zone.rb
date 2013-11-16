cb1452c042204c81281f7bde932b059cdf4c48a6
Mathias Biilmann Christensen <info@mathias-biilmann.net>
2013-11-15 18:16:07 -0800

    Add dns_records accessor to dns zone

--- lib/bitballoon/dns_zone.rb
+++ lib/bitballoon/dns_zone.rb
@@ -1,5 +1,11 @@
+require "bitballoon/dns_records"
+
 module BitBalloon
   class DnsZone < Model
     fields :id, :name, :user_id, :created_at, :updated_at
+
+    def dns_records
+      DnsRecords.new(client, path)
+    end
   end
 end
