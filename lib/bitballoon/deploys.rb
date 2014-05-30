e5871e999b7a52fa852115bbf455c912884ce694
Mathias Biilmann Christensen <info@mathias-biilmann.net>
2014-05-29 21:12:55 -0700

    Bump version to 0.2.0

--- lib/bitballoon/deploys.rb
+++ lib/bitballoon/deploys.rb
@@ -5,38 +5,17 @@ module BitBalloon
     path "/deploys"
 
     def create(attributes)
-      response = nil
       if attributes[:dir]
-        response = client.request(:post, path, :body => {:files => inventory(attributes[:dir])})
+        response = client.request(:post, path, :body => JSON.generate({:files => inventory(attributes[:dir])}))
+        Deploy.new(client, response.parsed).tap do |deploy|
+          deploy.upload_dir(attributes[:dir])
+        end
       elsif attributes[:zip]
         response = client.request(:post, path, :body => ::File.read(attributes[:zip]), :headers => {"Content-Type" => "application/zip"})
+        Deploy.new(client, response.parsed)
       else
         raise "Need dir or zip to create a deploy"
       end
-      Deploy.new(client, response.parsed)
-
-      #
-      # site_id = attributes.delete(:id)
-      # path    = site_id ? "/sites/#{site_id}" : "/sites"
-      # method  = site_id ? :put : :post
-      # if attributes[:dir]
-      #   dir = attributes.delete(:dir)
-      #   response = client.request(method, path, :body => JSON.generate(attributes.merge(:files => inventory(dir))), :headers => {"Content-Type" => "application/json"})
-      #   Site.new(client, response.parsed).tap do |site|
-      #     deploy = Deploy.new(client, :id => site.deploy_id, :required => site.required, :state => site.state)
-      #     puts "Uploading dir"
-      #     deploy.upload_dir(dir)
-      #     site.refresh
-      #   end
-      # elsif attributes[:zip]
-      #   zip = attributes.delete(:zip)
-      #   ::File.open(zip) do |file|
-      #     response = client.request(method, path, :body => attributes.merge(
-      #       :zip => Faraday::UploadIO.new(file, 'application/zip')
-      #     ))
-      #     Site.new(client, response.parsed)
-      #   end
-      # end
     end
 
     private
--- test/sites_test.rb
+++ test/sites_test.rb
@@ -18,22 +18,28 @@ class SitesTest < MiniTest::Unit::TestCase
 
     stub_request(:post, "https://www.bitballoon.com/api/v1/sites")
       .to_return {|request|
+        {
+          :headers => {'Content-Type' => 'application/json'},
+          :body => JSON.generate({:id => "1234"})
+        }
+      }
+    stub_request(:post, "https://www.bitballoon.com/api/v1/sites/1234/deploys")
+      .to_return {|request|
         body = JSON.parse(request.body)
         {
           :headers => {'Content-Type' => 'application/json'},
-         :body => JSON.generate({:id => "1234", :state => "uploading", :required => [index_sha]})
+          :body => JSON.generate({:id => "2345", :state => "uploading", :required => [index_sha]})
         }
       }
-    stub_request(:put, "https://www.bitballoon.com/api/v1/sites/1234/files/index.html")
-    stub_request(:get, "https://www.bitballoon.com/api/v1/sites/1234")
-      .to_return(:headers => {'Content-Type' => 'application/json'}, :body => {:id => "1234", :state => "processing"})
+    stub_request(:put, "https://www.bitballoon.com/api/v1/deploys/2345/files/index.html")
+    stub_request(:get, "https://www.bitballoon.com/api/v1/deploys/2345")
+      .to_return(:headers => {'Content-Type' => 'application/json'}, :body => {:id => "2345", :state => "processing"})
 
     site = client.sites.create(:dir => dir)
 
-    assert_equal 'processing', site.state
     assert_equal index_sha, body['files']['/index.html']
 
-    assert_requested :put, "https://www.bitballoon.com/api/v1/sites/1234/files/index.html",
+    assert_requested :put, "https://www.bitballoon.com/api/v1/deploys/2345/files/index.html",
       :body => ::File.read(::File.join(dir, "index.html")), :times => 1    # ===> Success
   end
 
@@ -41,12 +47,20 @@ class SitesTest < MiniTest::Unit::TestCase
     stub_request(:post, "https://www.bitballoon.com/api/v1/sites")
       .to_return({
         :headers => {'Content-Type' => 'application/json'},
-        :body => JSON.generate({:id => "1234", :state => "processing", :required => []})
+        :body => JSON.generate({:id => "1234"})
       })
 
+    stub_request(:post, "https://www.bitballoon.com/api/v1/sites/1234/deploys")
+    .to_return {|request|
+      {
+        :headers => {'Content-Type' => 'application/json'},
+        :body => JSON.generate({:id => "2345", :state => "uploading", :required => []})
+      }
+    }
     zip = ::File.expand_path("../files/site-dir.zip", __FILE__)
     site = client.sites.create(:zip => zip)
 
-    assert_equal 'processing', site.state
+    assert_requested :post, "https://www.bitballoon.com/api/v1/sites/1234/deploys",
+      :body => ::File.read(zip), :times => 1
   end
 end
