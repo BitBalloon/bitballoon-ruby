ebead08f9822b99c8becf01ac7d0103c299e3887
Mathias Biilmann Christensen <info@mathias-biilmann.net>
2014-05-29 21:23:07 -0700

    Fix content type when deploying dir

--- lib/bitballoon/deploys.rb
+++ lib/bitballoon/deploys.rb
@@ -6,12 +6,18 @@ module BitBalloon
 
     def create(attributes)
       if attributes[:dir]
-        response = client.request(:post, path, :body => JSON.generate({:files => inventory(attributes[:dir])}))
+        response = client.request(:post, path,
+          :body => JSON.generate({:files => inventory(attributes[:dir])}),
+          :headers => {"Content-Type" => "application/json"}
+        )
         Deploy.new(client, response.parsed).tap do |deploy|
           deploy.upload_dir(attributes[:dir])
         end
       elsif attributes[:zip]
-        response = client.request(:post, path, :body => ::File.read(attributes[:zip]), :headers => {"Content-Type" => "application/zip"})
+        response = client.request(:post, path,
+          :body => ::File.read(attributes[:zip]),
+          :headers => {"Content-Type" => "application/zip"}
+        )
         Deploy.new(client, response.parsed)
       else
         raise "Need dir or zip to create a deploy"
